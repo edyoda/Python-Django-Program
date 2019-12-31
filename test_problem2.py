ba93ae65a43fac8eaad470f07730e52a0e9abd7b
awantikdas <awantikdas@gmail.com>
2019-12-31 15:25:56 +0530

    cleanup

--- test_problem2.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from Problem2 import travel_sequence
-
-def test_travel_sequence():
-    travel_dict = {"Chennai":"Banglore","Bombay":"Delhi","Goa":"Chennai","Delhi":"Goa"}
-    assert travel_sequence(travel_dict) == ['Bombay->Delhi', 'Delhi->Goa', 'Goa->Chennai', 'Chennai->Banglore']
-
-
